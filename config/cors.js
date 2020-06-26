2a00e3d595314584f4f2c2f143e421bf9d6f5fac
João Luis Moraes <joaoluismoraes@Joaos-MacBook-Pro-2.local>
2020-06-26 15:43:03 -0300

    fix cors

--- config/cors.js
+++ config/cors.js
@@ -46,7 +46,7 @@ module.exports = {
   | Function - Receives the current header and should return one of the above values.
   |
   */
-  headers: ['*'],
+  headers: ['*', 'Authorization'],
 
   /*
   |--------------------------------------------------------------------------
