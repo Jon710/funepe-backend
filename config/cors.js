2ed1c2514331c1e06c3c0df81cff66aab75eeaf5
João Luis Moraes <joaoluismoraes@Joaos-MacBook-Pro-2.local>
2020-06-26 20:29:37 -0300

    fix cors

--- config/cors.js
+++ config/cors.js
@@ -1,58 +1,42 @@
 module.exports = {
   /*
-  |--------------------------------------------------------------------------
   | Origin
   |--------------------------------------------------------------------------
-  |
   | Set a list of origins to be allowed. The value can be one of the following
-  |
   | Boolean: true - Allow current request origin
   | Boolean: false - Disallow all
   | String - Comma separated list of allowed origins
   | Array - An array of allowed origins
   | String: * - A wildcard to allow current request origin
   | Function - Receives the current origin and should return one of the above values.
-  |
   */
   // origin: true,
   origin: ['https://funepe-frontend.herokuapp.com', 'http://localhost:3000/'],
 
   /*
-  |--------------------------------------------------------------------------
   | Methods
   |--------------------------------------------------------------------------
-  |
   | HTTP methods to be allowed. The value can be one of the following
-  |
   | String - Comma separated list of allowed methods
   | Array - An array of allowed methods
-  |
   */
   methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
 
   /*
-  |--------------------------------------------------------------------------
   | Headers
-  |--------------------------------------------------------------------------
-  |
   | List of headers to be allowed via Access-Control-Request-Headers header.
   | The value can be one of the following.
-  |
   | Boolean: true - Allow current request headers
   | Boolean: false - Disallow all
   | String - Comma separated list of allowed headers
   | Array - An array of allowed headers
   | String: * - A wildcard to allow current request headers
   | Function - Receives the current header and should return one of the above values.
-  |
   */
-  // headers: ['Authorization', 'Access-Control-Allow-Headers: Authorization'],
-  headers: ['https://funepe-frontend.herokuapp.com'],
+  headers: ['Authorization'],
+
   /*
-  |--------------------------------------------------------------------------
   | Expose Headers
-  |--------------------------------------------------------------------------
-  |
   | A list of headers to be exposed via `Access-Control-Expose-Headers`
   | header. The value can be one of the following.
   |
@@ -61,12 +45,10 @@ module.exports = {
   | Array - An array of allowed headers
   |
   */
-  exposeHeaders: ['https://funepe-frontend.herokuapp.com', 'Authorization'],
+  exposeHeaders: ['Content-Type', 'Authorization'],
 
   /*
-  |--------------------------------------------------------------------------
   | Credentials
-  |--------------------------------------------------------------------------
   |
   | Define Access-Control-Allow-Credentials header. It should always be a
   | boolean.
@@ -75,13 +57,8 @@ module.exports = {
   credentials: true,
 
   /*
-  |--------------------------------------------------------------------------
   | MaxAge
-  |--------------------------------------------------------------------------
-  |
   | Define Access-Control-Allow-Max-Age
-  |
   */
   maxAge: 90,
 };
-// dbfunepeinstance.c2jzriahor3a.sa-east-1.rds.amazonaws.com
