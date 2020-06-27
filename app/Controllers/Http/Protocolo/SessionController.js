8f850a01f3dfdd6d154ff7919c0549daeeb95712
João Luis Moraes <joaoluismoraes@Joaos-MacBook-Pro-2.local>
2020-06-27 18:41:25 -0300

    fix cors

--- app/Controllers/Http/Protocolo/SessionController.js
+++ app/Controllers/Http/Protocolo/SessionController.js
@@ -11,6 +11,10 @@ class SessionController {
         return response.status(401).json({ error: 'Senha incorreta.' });
       }
       const { token } = await auth.attempt(username, senha);
+      response.setHeader(
+        'Access-Control-Allow-Origin',
+        'https://funepe-frontend.herokuapp.com'
+      );
 
       return response.json({
         user,
