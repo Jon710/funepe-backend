7e9f75379ee613ec26a47992c4f37ac2bf3107c3
João Luis Moraes <joaoluismoraes@Joaos-MacBook-Pro-2.local>
2020-10-19 19:53:32 -0300

    fix query ItemOrcamentoController p/ MenorPreco

--- app/Controllers/Http/Compras/ItemOrcamentoController.js
+++ app/Controllers/Http/Compras/ItemOrcamentoController.js
@@ -54,14 +54,12 @@ class ItemOrcamentoController {
       from comp_itemorcamento as i, comp_orcamento as o, comp_produto as p, comp_fornecedor as f
       where (i.idorcamento = o.idorcamento and
         p.idproduto = i.idproduto and
-        i.valorunitario != 0 and
         f.idfornecedor = o.idfornecedor and
-        o.idrequisicao = ?) and i.valorunitario IN
-      (select min(i.valorunitario)
-      from comp_itemorcamento as i, comp_orcamento as o
-      group by i.idproduto
-      order by p.descricao)
-      `,
+        o.idrequisicao = ?) and
+        i.valorunitario in (select min(i.valorunitario)
+      from comp_itemorcamento as i
+      where i.valorunitario > 0
+      group by i.idproduto)`,
       [requisicao_id]
     );
 
