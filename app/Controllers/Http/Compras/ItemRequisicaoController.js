fed29b1993cff9f2c60f115ae1d9482cf0e7092a
moraes65 <moraes.jlcm@gmail.com>
2021-03-24 17:22:36 -0300

    fixing itensReq e Orcamento

--- app/Controllers/Http/Compras/ItemRequisicaoController.js
+++ app/Controllers/Http/Compras/ItemRequisicaoController.js
@@ -2,18 +2,35 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 const ItemRequisicao = use('App/Models/Compras/ItemRequisicao');
+const Database = use('Database');
 
 class ItemRequisicaoController {
   // /requisicao/:requisicao_id/itemrequisicao
   async index({ response, params }) {
     const { requisicao_id } = params;
-
+    console.log('ITENS: ');
     const itensrequisicao = await ItemRequisicao.query()
       .where('idrequisicao', requisicao_id)
       .with('requisicao')
       .with('produto')
       .with('produto.unidademedida')
       .fetch();
+
+    const itensReq = await Database.raw(
+      `select i.idproduto, i.idorcamento, i.quantidade, i.valorunitario, p.descricao, f.nomefantasia, i.valortotal
+      from comp_itemorcamento as i, comp_orcamento as o, comp_produto as p, comp_fornecedor as f
+      where (i.idorcamento = o.idorcamento and
+        p.idproduto = i.idproduto and
+        f.idfornecedor = o.idfornecedor and
+        o.idrequisicao = ?) and i.valortotal IN
+      (select min(i.valortotal) from comp_itemorcamento as i, comp_orcamento as o
+      group by i.idproduto
+      order by p.descricao)
+      `,
+      [requisicao_id]
+    );
+
+    console.log('ITENS: ', itensReq.rows);
 
     return response.json({
       itensrequisicao,
@@ -32,6 +49,7 @@ class ItemRequisicaoController {
 
   async show({ params, response }) {
     const { id } = params;
+    console.log('ITENS: ');
 
     const itemrequisicao = await ItemRequisicao.findOrFail(id);
 
